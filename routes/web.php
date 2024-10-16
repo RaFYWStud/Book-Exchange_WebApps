18152f91476700880c92287ae2f0e520474a09fb
Raffi Golonda <raffigolonda2006@gmail.com>
2024-10-16 10:40:38 +0800

    Memberi nama route dan memperbarui memberi route yang benar ke anchor navbar

--- routes/web.php
+++ routes/web.php
@@ -4,10 +4,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('Home');
-});
+})->name('home');
 
 Route::get('/OfferBook', function () {
     return view('OfferBook');
-});
+})->name('offerbook');
 
 Route::post('/books', [BookController::class, 'store'])->name('books.store');
